0d045fafd2f28076eb77bfb4e63a708e15b0d7cd
Andenor <rowebot98@gmail.com>
2021-11-17 19:32:22 -0700

    Add functions Join, Unionize, GetHeader, Size, CombineHeaders, IsJoinable, and CombineTuples

--- Relation.cpp
+++ Relation.cpp
@@ -77,8 +77,7 @@ Relation Relation::Project(std::vector<int> indices) {
     for (int i : indices) {
         vals.push_back(header.GetVal(i));
     }
-    Header newHead(vals);
-    Relation r(name, newHead);
+    Relation r(name, Header(vals));
 
     for (Tuple tup : tuples) {
         Tuple newTup;
@@ -98,4 +97,85 @@ Relation Relation::Rename(std::vector<std::string> newAttr) {
         r.AddTuple(t);
     }
     return r;
+}
+
+Relation Relation::Join(Relation r) {
+    Header newHeader = CombineHeaders(r.header);
+    Relation newRelation(name, newHeader);
+    for (auto t1 : tuples) {
+        for (auto t2 : r.tuples) {
+            if (IsJoinable(t1, t2, r.header)) {
+                newRelation.AddTuple(CombineTuples(t1, t2, r.header));
+            }
+        }
+    }
+    return newRelation;
+}
+Header Relation::CombineHeaders(Header h2) {
+    std::vector<std::string> newAttr;
+    for (int i = 0; i < header.Size(); ++i) {
+        newAttr.push_back(header.GetVal(i));
+    }
+    for (int i = 0; i < h2.Size(); ++i) {
+        bool shouldAdd = true;
+        for (auto s : newAttr) {
+            if (s == h2.GetVal(i)) {
+                shouldAdd = false;
+            }
+        }
+        if (shouldAdd) {
+            newAttr.push_back(h2.GetVal(i));
+        }
+    }
+    return Header(newAttr);
+}
+
+bool Relation::IsJoinable(Tuple t1, Tuple t2, Header h2) {
+
+    for (int i = 0; i < header.Size(); ++i) { // For each element in header 1
+        for (int j = 0; j < h2.Size(); ++j) { // For each element in header 2
+            if (header.GetVal(i) == h2.GetVal(j)) { // If the headers match:
+                if (t1.GetVal(i) != t2.GetVal(j)) { // If the values in each tuple don't match
+                    return false; // They can't be joined
+                }
+            }
+        }
+    } //If you make it all the way through:
+    return true; // They can be joined
+}
+
+Tuple Relation::CombineTuples(Tuple t1, Tuple t2, Header h2) {
+    Tuple newTuple;
+    for (int i = 0; i < header.Size(); ++i) {
+        newTuple.AddVal(t1.GetVal(i));
+    }
+    for (int i = 0; i < h2.Size(); ++i) {
+        bool isUnique = true;
+        for (int j = 0; j < header.Size(); ++j) {
+            if (h2.GetVal(i) == header.GetVal(j)) {
+                isUnique = false;
+                break;
+            }
+        }
+        if (isUnique) {
+            newTuple.AddVal(t2.GetVal(i));
+        }
+    }
+
+    return newTuple;
+}
+
+void Relation::Unionize(Relation r) {
+    for (Tuple t : r.tuples) {
+        if (tuples.insert(t).second) {
+            std::cout << "  ";
+            for (int i = 0; i < header.Size(); ++i) {
+                std::cout << header.GetVal(i) << "=" << t.GetVal(i);
+                if (i < header.Size() - 1) {
+                    std::cout << ", ";
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
 }
--- Relation.h
+++ Relation.h
@@ -16,17 +16,24 @@ public:
     void AddTuple(Tuple tup) {if (tup.Size() == header.Size()) tuples.insert(tup);}
     Relation Select(int i, std::string val);
     Relation Select(int i, int j);
+    Relation Join(Relation r);
+    void Unionize(Relation r); // maybe this should return a relation? not sure yet
     std::string ToString();
     std::string ToStringHeader();
     std::string ToStringTups();
     std::string GetAnswer();
     Relation Project(std::vector<int> indices); // list of indices
     Relation Rename(std::vector<std::string> newAttr); // list of attributes
+    Header GetHeader() {return header;}
+    int Size() {return tuples.size();}
 
 private:
     std::set<Tuple> tuples;
     Header header;
     std::string name;
+    Header CombineHeaders(Header h2);
+    bool IsJoinable(Tuple t1, Tuple t2, Header h2);
+    Tuple CombineTuples(Tuple t1, Tuple t2, Header h2);
 };
 
 
